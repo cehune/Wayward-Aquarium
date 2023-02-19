69fbaac187dfab62ca6f3b2f12cb3332510b06ae
mcka <mckale.chung@gmail.com>
2023-02-18 22:53:50 -0500

    first commit + web-scraper base

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const data = require('../web-scraper/fish_data')
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended:true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}))
+app.use(cors());
+
---
+++ web-scraper/fish_data.js
@@ -0,0 +1,55 @@
+const data = {
+    'Devil fish': {
+        title: 'Devil fish',
+        url: 'https://en.wikipedia.org/wiki/Devil_fish',
+        description: 'The devil fish or giant devil ray (Mobula mobular) is a species of ray in the family Mobulidae. It is currently listed as endangered, mostly due to bycatch mortality in unrelated fisheries.\n' +
+          'The devil fish is larger than its close relative the lesser devil ray. It grows to a length of disk 3.5 metres (11 ft), making it one of the largest rays. It possesses a spiny tail.[4] The devil fish is the third largest species in the genus Mobula, after the oceanic and reef manta rays. It is the only mobulid species that lives in the Mediterranean Sea. The species has been observed to have a maximum recorded length of disk width of 5.2 meters (roughly 17 feet).[5] However, those data are unreliable and are allegedly misunderstood as Giant oceanic Manta Rays that have strayed into the Mediterranean.[6][7] It is also considered to be the only Devil fish with a tail spine.[6][7] The species is also considered endangered given its decreasing population density.[8]\n' +
+          'The Devil fish is most common in the Mediterranean Sea. It is also found in the Eastern Atlantic Ocean, off the southwest coast of Ireland and south of Portugal, as well as in the central and western Pacific Ocean.[4] In the Mediterranean Sea, the species has a basin-wide distribution[8] and is most frequent in the eastern Mediterranean and the Adriatic Sea.[9] The large aggregations observed in the Levantine basin could signal an important mating area.[9]\n' +
+          'Devil fish inhabit offshore areas to the neritic zone, their range as deep as several thousand meters.  They are typically observed in small clusters, and may occasionally form larger groups.[1]\n' +
+          'Giant devil rays are usually seen in deep coastal waters but are occasionally seen in shallow waters. In a tagging experiment conducted by the Italian National Institute for Environmental Protection and Research (ISPRA), three giant devil rays were tagged and their depth was observed throughout different times of the day. The rays reached a maximum depth of 600–700 meters (1960–2300 feet) but mostly spent their time between 0 and 50 meters (0 and 165 feet); they prefer warmer waters with a temperature between 20 °C and 29 °C (68 °F and 84 °F). The giant devil rays also deep dive at random times, instances not correlated to the time of day unlike how other species deep dive at specific times of day.[5] In other observations studying ray abundance and habitat, giant devil rays were observed alone and occasionally in groups with a maximum of 18 rays. The same study also emphasizes that the rays undergo a species migration across the Mediterranean Sea with the seasons, taking advantage of warm, highly productive waters.[10]\n',
+        status: '\nEndangered (IUCN 3.1)[1]\n',
+        binomial: 'Mobula mobular',
+        img: '',
+        diet: 'Devil rays feed on planktonic crustaceans and small schooling fish, which are funneled into the mouth with cephalic flaps. When these flaps are rolled up, they look like horns, giving the animal a devil-like silhouette.[1] It mostly eats euphausiid shrimp (Meganyctiphanes norvegica) and small mesopelagic and clupeid fishes.[5]',
+        cuisine_img: "https://i.ytimg.com/vi/XVmHUmuVEg8/maxresdefault.jpg",
+        cuisine: "DATA ONLY AVAILABLE FOR STINGRAYS IN GENERAL. Stingrays are popular to eat in countries such as Malaysia and Singapore, and seen as a delicacy in many areas. They're commonly cooked over charcoal with spicy sambal sauce or soy sauce. The Pastel de chucho (Stingray Pie) is a popular dish is Eastern Venezuela.Generally, the most desired parts of the stingray are the fleshy wings, the cheek (the area surrounding the eyes), and the liver. The rest of the ray is considered too rubbery, due to the large amounts of cartilage, to have any other culinary uses."
+      },
+
+      'Yellowfin tuna': {
+        title: 'Yellowfin tuna',
+        url: 'https://en.wikipedia.org/wiki/Yellowfin_tuna',
+        description: 'The yellowfin tuna (Thunnus albacares) is a species of tuna found in pelagic waters of tropical and subtropical oceans worldwide.\n' +
+          'Yellowfin is often marketed as ahi, from the Hawaiian ʻahi, a name also used there for the closely related bigeye tuna.[3] The species name, albacares ("white meat") can also lead to confusion: in English, the albacore (Thunnus alalunga) is a different species, while yellowfin is officially designated albacore in French and referred to as albacora by Portuguese fishermen.\n' +
+          'The yellowfin tuna is among the larger tuna species, reaching weights over 180 kg (400 lb), but is significantly smaller than the Atlantic and Pacific bluefin tunas, which can reach over 450 kg (990 lb), and slightly smaller than the bigeye tuna and the southern bluefin tuna.\n' +
+          'The second dorsal fin and the anal fin, as well as the finlets between those fins and the tail, are bright yellow, giving this fish its common name. The second dorsal and anal fins can be very long in mature specimens, reaching almost as far back as the tail and giving the appearance of sickles or scimitars. The pectoral fins are also longer than the related bluefin tuna, but not as long as those of the albacore. The main body is a very dark metallic blue, changing to silver on the belly, which has about 20 vertical lines.\n' +
+          'Reported sizes in the literature have ranged as high as 2.4 m (7 ft 10 in) in length and 200 kg (440 lb) in weight. The all-tackle International Game Fish Association (IGFA) record for this species stands at 193.68 kg (427.0 lb) for a yellowfin caught in  2012 off Cabo San Lucas, Mexico. The fisherman received a prize of $1 million once the catch was confirmed by the IGFA.[4][5]\n',
+        status: '\nLeast Concern (IUCN 3.1)[1]',
+        binomial: 'Thunnus albacares',
+        img: '',
+        diet: 'Yellowfin tuna prey include other fish, pelagic crustaceans, and squid. Like all tunas, their body shape is particularly adapted for speed, enabling them to pursue and capture fast-moving baitfish such as flying fish, sauries, and mackerel. Schooling species such as myctophids or lanternfish and similar pelagic driftfish, anchovies, and sardines are frequently taken. Large yellowfins prey on smaller members of the tuna family such as frigate mackerel and skipjack tuna.',
+        cuisine: 'According to the Hawaii Seafood Buyers Guide, yellowfin tuna is widely used in raw fish dishes, especially sashimi. This fish is also excellent for grilling.[3] Yellowfin is often served seared rare.\n' +
+          '            Yellowfin buyers recognize two grades, "sashimi grade" and "other", although variation in the quality of "other" grades occurs.\n' +
+          `            Different seafood sustainability guides come to different conclusions about whether yellowfin fishing is sustainable. The Audubon's Seafood Guide (a guide for what types of marine food products are not ecofriendly) lists troll-caught tuna as "OK", but labels long-line caught as "Be Careful".[18]\n` +
+          '            Yellowfin is becoming a popular replacement for the severely depleted supplies of southern bluefin tuna. In 2010, Greenpeace International added the yellowfin tuna to its seafood red list. The Greenpeace International seafood red list is a "list of fish that are commonly sold in supermarkets around the world, and which have a very high risk of being sourced from unsustainable fisheries".',
+        cuisine_img: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Steack_de_thon_albacore_saut%C3%A9_et_flamb%C3%A9_au_cognac._Endives_et_c%C3%A9leri_branche_saut%C3%A9s.jpg'
+      },
+
+      'Blackfin tuna': {
+        title: 'Blackfin tuna',
+        url: 'https://en.wikipedia.org/wiki/Blackfin_tuna',
+        description: 'The blackfin tuna (Thunnus atlanticus) is a species of tuna in the family Scombridae. It is occasionally referred to as the bermuda tuna, blackfinned albacore, or deep bodied tunny.[3] They are the smallest tuna species in the genus Thunnus, generally growing to a maximum of 100 cm (39 in) in length and weighing 21 kg (46 lb).[4]\n' +
+          'Blackfin tuna are considered tropical, warm water fish. They have a relatively small distribution throughout the Eastern Coast of North and South America. They are located from Massachusetts to Rio de Janeiro in Brazil, and also inhabit the Gulf of Mexico and the Caribbean Sea.[5][2]\n' +
+          'The blackfin tuna has less commercial value than its close relatives like the yellowfin, bluefin, and skipjack tuna, but is still a major sport fish in Florida and regions of the Caribbean like Cuba and the Bahamas.[4][5] However, the taste and quality of blackfin tuna is comparable to that of its popular relatives, and can be prepared in the same ways. It is also sushi grade. Methods of capture include trolling, drift fishing, and sport fishing with a pole and live bait or lures like ballyhoo, mullet, small fishes or strip baits, feathers, jigs, or plugs.[3][5]\n' +
+          'As of 2010, blackfin tuna were deemed “least concern” by the IUCN.[4]\n' +
+          'Blackfin tuna are the smallest species of tuna. The All-Tackle world record blackfin tuna was caught off the coast of Florida and weighed 20.6 kg (45 lbs 8 ounces).[5]  Their oval shaped bodies have a dark blue to black colored back. The sides and belly of the fish are silver, which gives them an iridescent appearance in the water, with a lateral yellow stripe. They have durable, compact scales with an oblique mouth.[5][2]\n',
+        status: '\nLeast Concern (IUCN 3.1)[1]\n',
+        binomial: 'Thunnus atlanticus',
+        img: '',
+        diet: 'Yellowfin tuna prey include other fish, pelagic crustaceans, and squid. Like all tunas, their body shape is particularly adapted for speed, enabling them to pursue and capture fast-moving baitfish such as flying fish, sauries, and mackerel. Schooling species such as myctophids or lanternfish and similar pelagic driftfish, anchovies, and sardines are frequently taken. Large yellowfins prey on smaller members of the tuna family such as frigate mackerel and skipjack tuna.',
+        cuisine: 'Blackfin tuna hunt both epipelagic (surface) and mesopelagic (deeper water) fish and squid. They also eat crustaceans such as shrimp, crabs, amphipods, stomatopods, and the larvae of decapods.[2][3] They consume smaller prey by filtering the water and chase to catch larger prey.[5]',
+        cuisine_img: 'https://i0.wp.com/helpfulcaptain.com/wp-content/uploads/2019/11/Pan-Seared-Blackfin-Tuna-ready-to-eat-scaled.jpg?resize=1280%2C640&ssl=1'
+      },
+
+};
+
+module.exports = {data}
