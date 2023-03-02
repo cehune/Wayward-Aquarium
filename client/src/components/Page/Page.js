50cfaa3c4144d3165f290c8a921f2babe9b8d20b
mcka <mckale.chung@gmail.com>
2023-03-01 22:18:53 -0500

    Favourites fully functional

--- client/src/components/Page/Page.js
+++ client/src/components/Page/Page.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Gallery } from './Gallery/Gallery';
 import { Info } from './Info/Info';
 import { Button } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './page.scss'
-
+import { useState } from 'react';
 import { buttonStyleActive, buttonStyleAsleep } from './PageStyles';
+
+
 
 export const Page = ({ info, favourites, setFavourites,seeInfo, setSeeInfo }) => {
 
+  const [liked, setLiked] = useState(favourites.includes(info.name))
   return (
 
     <div><div className='page'>
@@ -23,12 +27,26 @@ export const Page = ({ info, favourites, setFavourites,seeInfo, setSeeInfo }) =>
 
       </div>
       <h4 className='scientific'>{info.scientific}</h4>
-      <button onClick ={()=> {
-        if (!favourites.includes(info.name)) {
-          setFavourites = favourites.push(info.name)
-        }
-        console.log(favourites)
-      }}> Save as favourite</button>
+      <div className='favourite'>
+          <Button sx={favourites.includes(info.name) ?  {border: "solid 1px red"} : {border: "solid 1px grey"}}
+          
+          onClick={()=> {
+            if (!favourites.includes(info.name)) {
+              setFavourites= favourites.push(info.name)
+              setLiked(true)
+            }
+            else {
+              setFavourites=favourites.splice(favourites.indexOf(info.name), 1)
+              setLiked(false)
+            }
+            console.log(liked)
+          }}>
+              <FavoriteIcon sx={favourites.includes(info.name) ?  {color: "red"} : {color: "grey"}}/>
+          </Button>
+          {favourites.includes(info.name) ? <p>Favourited</p> : <p>Set to Favourites</p>}
+
+      </div>
+    
 
       <hr />
 
@@ -38,7 +56,7 @@ export const Page = ({ info, favourites, setFavourites,seeInfo, setSeeInfo }) =>
         }}
         sx={seeInfo ? buttonStyleActive : buttonStyleAsleep}
       >info</Button>
-      
+
       <Button variant='outlined'
         onClick={() => { //switch to the info section
           if (seeInfo === true) { setSeeInfo(false) }
